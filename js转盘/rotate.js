ef8eef18eca96ddaf34346495c840865dc7a035d
gavan_li <pa617372@163.com>
2019-10-29 20:48:03 +0800

    hytyt

--- js转盘/rotate.js
+++ js转盘/rotate.js
@@ -1,59 +1,72 @@
-!function() {
+;(function(window) {
   function Rotate(config) {
     var options = {
       steps: [],
-      totalDeg: 360 * 5,
-      a: 0.01,
-      now: 0,
       running: false,
       prize: null,
-      outEl: document.getElementById('outer')
+      timer: null,
+      // outEl: document.getElementById('outer')
     };
     this.options = Object.assign(options, config);
-    this.init();
+    // this.countSteps();
   }
   Rotate.prototype = {
     init: function() {
       this.countSteps();
       this.animCallback(this.step);
     },
-    // 组装step算法
-    countSteps: function() {
+    start: function() {
+      var config = this.options,
+          i = 10;
+      if (config.running) return;
+      config.running = true;
+      
+      config.timer = setInterval(function () {
+        i += 6;
+        config.outEl.style.webkitTransform = 'rotate3d(0,0,1,'+i+'deg)';
+      }, 10)
+    },
+    end: function(deg) {
       var config = this.options;
-      var t = Math.sqrt(2 * config.totalDeg / config.a);
-      var v = config.a * t;
+      // if (config.running) return;
+      // config.running = true;
+      //重置
+      config.now = 0;
+      config.steps = [];
+
+      this.countSteps(deg);
+      // 设置停止角度
+      config.timer && clearInterval(config.timer);
+      this.animCallback(this.step);
+    },
+    // 组装step算法
+    countSteps: function(deg = 0) {
+      var config = this.options,
+          totalDeg = 360 * 3 + deg,
+          a = 0.01,
+          t = Math.sqrt(2 * totalDeg / a),
+          v = a * t;
+
       for (var i = 0; i < t; i++) {
-        config.steps.push((2 * v * i - config.a * i * i) / 2)
+        config.steps.push((2 * v * i - a * i * i) / 2)
       }
-      config.steps.push(config.totalDeg)
+      config.steps.push(totalDeg)
     },
     animCallback: function(callback) {
       window.setTimeout(callback.bind(this), 1000 / 60)
     },
     step: function() {
       var config = this.options;
-      config.outEl.style.webkitTransform = 'rotate(' + config.steps[config.now++] + 'deg)';
+
+      config.outEl.style.webkitTransform = 'rotate3d(0,0,1,'+config.steps[config.now++]+'deg)';
       // outter.style.MozTransform = 'rotate(' + steps[now++] + 'deg)';
       if (config.now < config.steps.length) {
-          this.animCallback(this.step)
+        this.animCallback(this.step);
       } else {
         config.running = false;
-        setTimeout(function () {
-          if (config.prize != null) {
-            var type = "";
-            if (config.prize == 1) {
-              type = "一等奖"
-            } else if (prize == 2) {
-              type = "二等奖"
-            } else if (prize == 3) {
-              type = "三等奖"
-            }
-          } else {
-            // alert("谢谢您的参与，下次再接再厉")
-          }
-        }, 200)
       }
     }
   }
-  new Rotate();
-}()
+
+  window.Rotate = Rotate;
+}(window))
