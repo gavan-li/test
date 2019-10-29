41f9e10fd38edef65ec42309f3cd29bd487c428f
bingleey <pa617372@163.com>
2019-10-30 01:19:22 +0800

    geigre

--- js转盘/rotate.js
+++ js转盘/rotate.js
@@ -3,7 +3,6 @@
     var options = {
       steps: [],
       running: false,
-      prize: null,
       timer: null,
       // outEl: document.getElementById('outer')
     };
@@ -11,10 +10,10 @@
     // this.countSteps();
   }
   Rotate.prototype = {
-    init: function() {
+    /*init: function() {
       this.countSteps();
       this.animCallback(this.step);
-    },
+    },*/
     start: function() {
       var config = this.options,
           i = 10;
@@ -22,25 +21,25 @@
       config.running = true;
       
       config.timer = setInterval(function () {
-        i += 6;
+        i += 4;
         config.outEl.style.webkitTransform = 'rotate3d(0,0,1,'+i+'deg)';
-      }, 10)
+      }, 5)
     },
-    end: function(deg) {
+    end: function(prize) {
       var config = this.options;
-      // if (config.running) return;
-      // config.running = true;
       //重置
       config.now = 0;
       config.steps = [];
 
-      this.countSteps(deg);
+      this.countSteps(prize);
       // 设置停止角度
       config.timer && clearInterval(config.timer);
+
       this.animCallback(this.step);
     },
     // 组装step算法
-    countSteps: function(deg = 0) {
+    countSteps: function(prize) {
+      var deg = this.getPrize(prize);
       var config = this.options,
           totalDeg = 360 * 3 + deg,
           a = 0.01,
@@ -53,7 +52,7 @@
       config.steps.push(totalDeg)
     },
     animCallback: function(callback) {
-      window.setTimeout(callback.bind(this), 1000 / 60)
+      setTimeout(callback.bind(this), 6)
     },
     step: function() {
       var config = this.options;
@@ -65,6 +64,16 @@
       } else {
         config.running = false;
       }
+    },
+    getPrize: function(type) {
+      //type必填
+      var section = this.options.section[type],
+          pareceRate = section.split('~'),
+          minRate = parseInt(pareceRate[0]),
+          maxRate = parseInt(pareceRate[1]);
+      return parseInt(Math.random()*(maxRate-minRate+1)+minRate, 10);
+      /*if (!type ) {return parseInt(Math.random()*360+1, 10);
+      } else {}*/
     }
   }
 
