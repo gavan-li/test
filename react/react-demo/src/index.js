01392c40da30e1ccfda9c501de58be19b6ea5d5c
gavan_li <pa617372@163.com>
2020-07-13 18:31:00 +0800

    45185

--- react/react-demo/src/index.js
+++ react/react-demo/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
